beea7e0d577ee3eb9f4f6e1bfc68eb0089272552
brenman60 <83514481+brenman60@users.noreply.github.com>
2023-12-13 15:33:41 -0500

    Visual changes

--- brenman60's Modpack Manager/MainWindow.xaml.cs
+++ brenman60's Modpack Manager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace brenman60_s_Modpack_Manager
 {
@@ -7,11 +9,47 @@ namespace brenman60_s_Modpack_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Grid> tabContents = new List<Grid>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            tabContents.Add(launcherContent);
+            tabContents.Add(modpacksContent);
+            tabContents.Add(modSettingsContent);
         }
 
+        private void SwitchTab(object sender, RoutedEventArgs e)
+        {
+            Button? clickedButton = sender as Button;
+            if (clickedButton == null)
+                return;
 
+            switch (clickedButton.Name)
+            {
+                case "launcherButton":
+                    ChangeTabVisiblity(launcherContent);
+                    break;
+                case "modpacksButton":
+                    ChangeTabVisiblity(modpacksContent);
+                    break;
+                case "modSettingsButton":
+                    ChangeTabVisiblity(modSettingsContent);
+                    break;
+                default:
+                    ChangeTabVisiblity(launcherContent);
+                    break;
+            }
+        }
+
+        private void ChangeTabVisiblity(Grid selectedTab)
+        {
+            foreach (Grid item in tabContents)
+                if (item != selectedTab)
+                    item.Visibility = Visibility.Collapsed;
+                else
+                    item.Visibility = Visibility.Visible;
+        }
     }
 }
