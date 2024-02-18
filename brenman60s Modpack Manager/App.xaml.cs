b590617839ceba6fed91f2145e81afe4978a1076
brenman60 <83514481+brenman60@users.noreply.github.com>
2024-02-18 13:10:37 -0500

    Version 1.1.1

--- brenman60s Modpack Manager/App.xaml.cs
+++ brenman60s Modpack Manager/App.xaml.cs
@@ -1,13 +1,26 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Diagnostics;
+using brenman60_s_Modpack_Manager.Scripts;
+using ModpackManager.Utils;
+using Newtonsoft.Json;
 
 namespace brenman60_s_Modpack_Manager
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
+            FileManager fileManager = new FileManager();
+            Downloader downloader = new Downloader();
+            string dd_downloadedFile = await downloader.DownloadFile("https://dl.dropboxusercontent.com/scl/fi/ap7lmeh2plcchfpl4x1bt/directModDownloads.txt?rlkey=4clebota1r6q1ss7661rripyb&dl=0", new Progress<int>(), ".txt");
+            string dd_downloadedFileContent = fileManager.ReadFile(dd_downloadedFile);
+            Mods.directDownloads = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(dd_downloadedFileContent);
+
+            string msi_downloadedFile = await downloader.DownloadFile("https://dl.dropboxusercontent.com/scl/fi/a4omdo8c06mev7qih03g8/modSettingsInfo.txt?rlkey=nsoq5rdxlrg1occpyoec1ie0o&dl=0", new Progress<int>(), ".txt");
+            string msi_downloadedFileContet = fileManager.ReadFile(msi_downloadedFile);
+            Mods.modSettingsInfo = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, Dictionary<string, string>>>>>(msi_downloadedFileContet);
+
             if (e.Args.Length == 0)
             {
                 bool startUpdater = true;
